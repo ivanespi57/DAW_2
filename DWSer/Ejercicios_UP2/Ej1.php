12f86726c3c02f2ea66a175f56494294232a7575
Ivan <ivanespiasins75@gmail.com>
2025-10-06 19:55:35 +0200

    Update

--- DWSer/Ejercicios_UP2/Ej1.php
+++ DWSer/Ejercicios_UP2/Ej1.php
@@ -1,22 +1,19 @@
 <?php
-    function potencia($n, $x){
-        
-        $potencias = [];
+function potencias($n, $x) {
+    $potencias = [];
 
-        for($i = 0; $i <= $x; $i++){
-            $potencias[] = pow($n,$i);
-        }
-        
+    for ($i = 1; $i <= $x; $i++) {
+        $potencias[] = pow($n, $i);
     }
-    potencia(7, 10);
-    $suma = 0;
 
-    foreach($potencias as $i => $v){
-        echo "Potencia " . ($i+1) . " = $v <br>";
+    $suma = 0;
+    foreach ($potencias as $i => $v) {
+        echo "Potencias " . ($i + 1) . " = $v<br>";
         $suma += $v;
     }
 
-    echo "Suma de las potencias: $suma";
+    echo "Suma de todas las potencias: $suma";
+}
 
-    
+potencias(3, 6);
 ?>
