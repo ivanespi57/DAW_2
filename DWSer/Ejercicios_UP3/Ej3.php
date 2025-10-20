bf0749d506adbe1afc8f7054cc1602f7ed7d7530
ivanespi57 <ivanespiasins75@gmail.com>
2025-10-20 15:39:32 +0200

    Update

--- DWSer/Ejercicios_UP3/Ej3.php
+++ DWSer/Ejercicios_UP3/Ej3.php
@@ -7,22 +7,35 @@
 <body>
     <h1>Iván Espí Asins</h1>
     <hr>
-    <h2>Calcular salario semanal</h2>
+    <h2>Comprobar tipo de carácter (con funciones ctype)</h2>
 
     <form method="post">
-        <label>Nombre del trabajador:</label>
-        <input type="text" name="nombre"><br><br>
-        <label>Nº de horas a la semana:</label>
-        <input type="number" name="horas"><br><br>
-
-        <input type="checkbox" name="salMens" id="salMens">Calcular salario mensual<br><br>
-
-        <input type="submit" value="Calcular"><br><br>
-        
+        <label>Introduce un carácter:</label>
+        <input type="text" name="caracter" maxlength="1" required><br><br>
+        <input type="submit" value="Comprobar">
     </form>
 
     <?php
-        
+        if ($_POST) {
+            $car = $_POST['caracter'];
+
+            echo "<hr>";
+            echo "<p>El carácter introducido es: <strong>$car</strong></p>";
+
+            if (ctype_upper($car)) {
+                echo "<p>Es una letra <strong>mayúscula</strong>.</p>";
+            } elseif (ctype_lower($car)) {
+                echo "<p>Es una letra <strong>minúscula</strong>.</p>";
+            } elseif (ctype_digit($car)) {
+                echo "<p>Es un carácter <strong>numérico</strong>.</p>";
+            } elseif (ctype_space($car)) {
+                echo "<p>Es un carácter <strong>en blanco</strong>.</p>";
+            } elseif (ctype_punct($car)) {
+                echo "<p>Es un carácter <strong>de puntuación</strong>.</p>";
+            } else {
+                echo "<p>Es un carácter <strong>especial o no imprimible</strong>.</p>";
+            }
+        }
     ?>
 </body>
 </html>
