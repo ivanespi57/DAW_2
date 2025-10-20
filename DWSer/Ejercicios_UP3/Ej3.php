f238485fcef25e22da60e5444c3eae0f1433d9e3
Ivan <ivanespiasins75@gmail.com>
2025-10-20 15:38:53 +0200

    Update

---
+++ DWSer/Ejercicios_UP3/Ej3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Iván Espí Asins</title>
+</head>
+<body>
+    <h1>Iván Espí Asins</h1>
+    <hr>
+    <h2>Calcular salario semanal</h2>
+
+    <form method="post">
+        <label>Nombre del trabajador:</label>
+        <input type="text" name="nombre"><br><br>
+        <label>Nº de horas a la semana:</label>
+        <input type="number" name="horas"><br><br>
+
+        <input type="checkbox" name="salMens" id="salMens">Calcular salario mensual<br><br>
+
+        <input type="submit" value="Calcular"><br><br>
+        
+    </form>
+
+    <?php
+        
+    ?>
+</body>
+</html>
