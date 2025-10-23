b6e06bc9bf7acd51e067fa101366802c789bec08
Ivan <ivanespiasins75@gmail.com>
2025-10-23 12:35:38 +0200

    Update

---
+++ PIM/js/app.js
@@ -0,0 +1,28 @@
+import {Controlador} from './controlador.js';
+import {Vista} from './vista.js';
+
+
+const iniciarAplicacion = async () => {
+const vista = new Vista({mapId: 'mapa'});
+const controlador = new Controlador({vista});
+
+
+const rango = document.getElementById('num-estaciones');
+const rangoVal = document.getElementById('num-estaciones-val');
+const btnRef = document.getElementById('btn-refrescar');
+
+
+rango.addEventListener('input', () => {
+rangoVal.textContent = rango.value;
+controlador.setMaxEstaciones(Number(rango.value));
+});
+
+
+btnRef.addEventListener('click', () => controlador.refrescar());
+
+
+await controlador.iniciarAplicacion();
+};
+
+
+window.addEventListener('DOMContentLoaded', iniciarAplicacion);
