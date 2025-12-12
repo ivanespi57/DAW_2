33283dfe7199152e7ab408402d59322778b36710
Ivan <ivanespiasins75@gmail.com>
2025-12-12 18:53:52 +0100

    Update

--- DWCl/Actividades/BarraProgreso/index.js
+++ DWCl/Actividades/BarraProgreso/index.js
@@ -1,15 +1,22 @@
 let porcentajeCarga = 0;
 const barra = document.querySelector(".barraFront");
 const completo = document.querySelector("h1");
+const cont = document.querySelector(".contador");
 
 const intervalo = setInterval(() => {
 
     porcentajeCarga += 1;
     barra.style.width = porcentajeCarga + "%";
+    cont.textContent = porcentajeCarga + "%";
 
     if (porcentajeCarga === 100){
         clearInterval(intervalo);
         barra.style.width = "100%";
         completo.textContent = "Carga completada"
     }
+
+    if(porcentajeCarga > 80){
+        barra.classList.add("alerta-final");
+    }
+
 }, 50);
